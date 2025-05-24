a04ae1cda8f56638489e549cf0371253318564e3
Arian khan 21078101 <as8khan@uwaterloo.ca>
2025-05-24 19:35:55 -0400

    changed titles

--- src/data/events.js
+++ src/data/events.js
@@ -1,14 +1,14 @@
 const events = [
     {
         title: 'CoinWa',
-        role: 'Ai Developer',
+        role: 'Ai Engineering Intern',
         date: "May 2025 - Aug 2025",
         description: "At CoinWa, I’m leading the development of an intelligent, AI system for financial literacy education. The system uses deep learning and NLP to generate personalized learning content and dynamically adapt lesson difficulty based on each user's strengths and weaknesses. I’m building models for user skill tracking and integrating transformer-based NLP for exercise generation. My work spans model training (PyTorch & Hugging Face), user modeling, and deployment of AI APIs that drive real-time learning experiences tailored to each learner.",
         image: '/assets/images/coinwa.jpg',
     },
     {
         title: 'CoinWa',
-        role: 'Software Engineer',
+        role: 'Software Engineering Intern',
         date: "Jan 2025 - Apr 2025",
         description: 'Developed a financial literacy app using Flutter and Dart to educate 13-18-year-olds on financial concepts, incorporating features like login, registration, email verification, password recovery, and structured learning modules. Designed responsive UI layouts with Flutter, ensuring compatibility across devices, and used Canva to create engaging visuals. Integrated a secure backend with Node.js and MongoDB to manage user authentication and data storage. Enhanced user engagement with interactive quizzes, mini-games, and lessons. Collaborated with a team using Git and GitHub for version control and optimized performance with Flutter DevTools, improving responsiveness and fixing bugs.',
         image: '/assets/images/coinwa.jpg',
